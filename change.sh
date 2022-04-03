8a5a56fb86f5c8afbc5f455a43e2a5f10d784b9c
nourhankhalifa <85039864+nourhankhalifa@users.noreply.github.com>
2022-04-03 16:58:35 +0200

    Create change.sh

---
+++ change.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sed -i 's/145/0,.1/g' site.css
+sed -i 's/98/0/g' site.css
+sed -i 's/41/0/g' site.css
+sed -i 's/rgb/rgba/g' site.css
+sed -i 's/380a66/366ec3/g' site.css
+sed -i 's/Board/Board Demo!/g' index.html
